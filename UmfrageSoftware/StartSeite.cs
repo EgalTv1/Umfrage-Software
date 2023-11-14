8c3f405062da57afa4824147b792d283ddf72e4e
Johann Hoffmam <johannhoffmam@gmail.com>
2023-11-14 12:51:07 +0100

    Start Seite

--- UmfrageSoftware/StartSeite.cs
+++ UmfrageSoftware/StartSeite.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace UmfrageSoftware
 {
-    public partial class Form1 : Form
+    public partial class StartSeite : Form
     {
-        public Form1()
+        public StartSeite()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
