5c316ee2bec466f69280370ccfbf138f89c98571
Johann :) <johannhoffmam@gmail.com>
2023-11-17 22:42:06 +0100

    Umfrage Erstellen

--- UmfrageSoftware/Umfrage Erstellen.Designer.cs
+++ UmfrageSoftware/Umfrage Erstellen.Designer.cs
@@ -116,7 +116,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 184);
+            this.button2.Location = new System.Drawing.Point(104, 184);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
