96d987ba5f3e0b109630168d74afb322d794ff6f
EgalTv <fabianbecker727@gmail.com>
2023-11-25 13:31:52 +0100

    Tabellen Name und Beschreibung wird in MEssagbox ausgegeben

--- UmfrageSoftware/ModelUmfragenListe.cs
+++ UmfrageSoftware/ModelUmfragenListe.cs
@@ -1,43 +1,60 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace UmfrageSoftware
 {
     internal class ModelUmfragenListe
     {
-
-        public static List<string> UmfragenSammeln()
+        public static void UmfragenSammeln()
         {
             List<string> UmfragenTabellen = new List<string>();
             MySqlConnection connection = DatenbankVerbindung.DatenbankVerbinden();
+
             if (connection != null)
             {
                 MySqlCommand CommandUmfragenTabellen = connection.CreateCommand();
                 CommandUmfragenTabellen.CommandText = "SHOW TABLES LIKE 'UMF\\_%';";
 
-                MySqlDataReader TabellenReader = CommandUmfragenTabellen.ExecuteReader();
-                while (TabellenReader.Read())
+                using (MySqlDataReader TabellenReader = CommandUmfragenTabellen.ExecuteReader())
                 {
-                    UmfragenTabellen.Add(TabellenReader.ToString());
+                    while (TabellenReader.Read())
+                    {
+                        string tableNameWithPrefix = TabellenReader.GetString(0);
+                        UmfragenTabellen.Add(tableNameWithPrefix);
+                    }
                 }
 
+                MessageBox.Show(string.Join(Environment.NewLine, UmfragenTabellen));
 
+                List<Umfrage> UmfragenData = new List<Umfrage>();
 
+                foreach (string tableName in UmfragenTabellen)
+                {
+                    MySqlCommand CommandUmfragenDaten = connection.CreateCommand();
+                    CommandUmfragenDaten.CommandText = $"SELECT UmfragenBeschreibung FROM {tableName};";
 
+                    using (MySqlDataReader DatenReader = CommandUmfragenDaten.ExecuteReader())
+                    {
+                        while (DatenReader.Read())
+                        {
+                            Umfrage umfrage = new Umfrage
+                            {
+                                UmfragenBeschreibung = DatenReader.IsDBNull(0) ? null : DatenReader.GetString(0),
+                                UmfragenName = tableName.Substring(4), // Entferne das Präfix "UMF_"
+                            };
+
+                            UmfragenData.Add(umfrage);
+                        }
+                    }
+                }
+
+                foreach (Umfrage umfrage in UmfragenData)
+                {
+                    MessageBox.Show($"UmfragenName: {umfrage.UmfragenName}, UmfragenBeschreibung: {umfrage.UmfragenBeschreibung}");
+                }
             }
-
-
-            return null;
-
         }
-
-
-
-
-
     }
 }
